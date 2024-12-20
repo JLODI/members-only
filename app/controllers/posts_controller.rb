7976389388a2c26ecd29e7694a660567790a986e
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-19 17:16:36 -0800

    added index, new and create, and authentication in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_member!
+
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        redirect_to index
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  private
+    def post_params
+    params.expect(post: [:title, :body, :member_id])
+  end
+
+
 end
