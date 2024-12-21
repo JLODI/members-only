67e240728da0ef5ef1c80f1d012abfd7e85389bd
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-20 22:52:57 -0800

    added validations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :member
+  validates :title, :body, presence: true
+  validates :title, :body, { minimum: 3, message: "must be atleast 3 characters in length"}
 end
